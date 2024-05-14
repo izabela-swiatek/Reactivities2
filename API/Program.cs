5439e022fc27cfc2f00a13d98b77a8b1e5b61240
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2024-05-14 13:11:39 +0200

    End of section 12

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,10 @@ using API.Extensions;
 using API.Middleware;
 using Application.Activities;
 using Application.Core;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -9,8 +13,13 @@ var services = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-services.Services.AddControllers();
+services.Services.AddControllers(opt => 
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 services.Services.AddApplicationServices(services.Configuration);
+services.Services.AddIdentityServices(services.Configuration);
 
 var app = services.Build();
 
@@ -25,6 +34,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -35,8 +45,9 @@ var service = scope.ServiceProvider;
 try
 {
     var context =  service.GetRequiredService<DataContext>();
+    var userManager =  service.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
